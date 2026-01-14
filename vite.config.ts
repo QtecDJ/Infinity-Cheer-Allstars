21554df4affbad9d93220179cedab782ea58f193
Infinity Cheer Allstars <info@infinity-cheer-allstars.de>
2026-01-14 13:02:27 +0100

    Add custom domain: web.icacheer.space

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Infinity-Cheer-Allstars/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
