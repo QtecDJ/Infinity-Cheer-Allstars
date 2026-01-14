8b9ab3d22623a3276caaa62ba646eca5c859e37d
Infinity Cheer Allstars <info@infinity-cheer-allstars.de>
2026-01-14 12:48:38 +0100

    Fix: Korrigiere base URL für GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/infinity-cheer-allstars/',
+  base: '/Infinity-Cheer-Allstars/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
